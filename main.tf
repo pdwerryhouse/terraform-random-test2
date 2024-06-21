fb17b43953c6f398e1ed25b07fc03778f2515f11
Paul Dwerryhouse <paul@dwerryhouse.com.au>
2024-06-21 16:28:16 +1000

    fix

--- main.tf
+++ main.tf
@@ -3,10 +3,3 @@ resource "random_string" "this" {
   length           = var.length
   special          = false
 }
-
-module "blah" {
-  count           = var.recurse == false ? 0 : 1
-  source          = "git::https://github.com/pdwerryhouse/terraform-random-test.git"
-  length          = var.length
-  recurse         = false
-}
--- variables.tf
+++ variables.tf
@@ -1,5 +1,1 @@
-variable "recurse" { 
-  type = bool
-  default = false
-}
 variable "length" { default = 16 }
